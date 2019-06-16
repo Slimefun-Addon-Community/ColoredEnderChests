a483869a845e338ddd99ca97fdb65f12c3455acc
TheBusyBiscuit <mrCookieSlime@gmail.com>
2019-06-16 13:44:28 +0200

    Initial Commit

---
+++ src/io/github/thebusybiscuit/coloredenderchests/ColoredEnderChest.java
@@ -0,0 +1,122 @@
+package io.github.thebusybiscuit.coloredenderchests;
+
+import java.util.stream.IntStream;
+
+import org.bukkit.Material;
+import org.bukkit.Sound;
+import org.bukkit.block.Block;
+import org.bukkit.block.data.type.EnderChest;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import me.mrCookieSlime.CSCoreLibPlugin.general.Inventory.Item.CustomItem;
+import me.mrCookieSlime.Slimefun.Lists.RecipeType;
+import me.mrCookieSlime.Slimefun.Lists.SlimefunItems;
+import me.mrCookieSlime.Slimefun.Objects.SlimefunBlockHandler;
+import me.mrCookieSlime.Slimefun.Objects.SlimefunItem.SlimefunItem;
+import me.mrCookieSlime.Slimefun.Objects.SlimefunItem.UnregisterReason;
+import me.mrCookieSlime.Slimefun.api.BlockStorage;
+import me.mrCookieSlime.Slimefun.api.inventory.BlockMenu;
+import me.mrCookieSlime.Slimefun.api.inventory.BlockMenuPreset;
+import me.mrCookieSlime.Slimefun.api.item_transport.ItemTransportFlow;
+
+public class ColoredEnderChest extends SlimefunItem {
+
+	public ColoredEnderChest(ColoredEnderChests plugin, int size, int c1, int c2, int c3) {
+		super(plugin.category, 
+			new CustomItem(Material.ENDER_CHEST, "&eColored Ender Chest &7(" + (size == 27 ? "Small": "Big") + ")", "", "&7Size: &e" + size, "", "&7- " + plugin.colors.get(c1), "&7- " + plugin.colors.get(c2), "&7- " + plugin.colors.get(c3)), 
+			"COLORED_ENDER_CHEST_" + (size == 27 ? "SMALL": "BIG") + "_" + c1 + "_" + c2 + "_" + c3, 
+			RecipeType.ANCIENT_ALTAR,
+			(size == 27) ?
+				new ItemStack[] {
+					new ItemStack(plugin.wool[c1]), new ItemStack(plugin.wool[c2]), new ItemStack(plugin.wool[c3]), 
+					new ItemStack(Material.OBSIDIAN), new ItemStack(Material.CHEST), new ItemStack(Material.OBSIDIAN), 
+					SlimefunItems.RUNE_ENDER, new ItemStack(Material.OBSIDIAN), SlimefunItems.RUNE_ENDER
+				}
+			:
+				new ItemStack[] {
+					new ItemStack(plugin.wool[c1]), new ItemStack(plugin.wool[c2]), new ItemStack(plugin.wool[c3]), 
+					SlimefunItems.WITHER_PROOF_OBSIDIAN, SlimefunItem.getItem("COLORED_ENDER_CHEST_SMALL_" + c1 + "_" + c2 + "_" + c3), SlimefunItems.WITHER_PROOF_OBSIDIAN, 
+					SlimefunItems.RUNE_ENDER, SlimefunItems.GOLD_24K, SlimefunItems.RUNE_ENDER
+				}
+		);
+		
+		int[] slots = IntStream.range(0, size).toArray();
+		
+		SlimefunItem.registerBlockHandler(getID(), new SlimefunBlockHandler() {
+			
+			@Override
+			public void onPlace(Player p, Block b, SlimefunItem item) {
+				int yaw = 0;
+				
+				EnderChest chest = (EnderChest) b.getBlockData();
+				
+				switch (chest.getFacing()) {
+					case NORTH:
+						yaw = 180;
+						break;
+					case SOUTH:
+						yaw = 0;
+						break;
+					case WEST:
+						yaw = 90;
+						break;
+					case EAST:
+						yaw = -90;
+					default:
+						break;
+				}
+				
+				BlockStorage.addBlockInfo(b, "yaw", String.valueOf(yaw));
+				plugin.updateIndicator(b, c1, c2, c3, yaw + 45);
+			}
+			
+			@Override
+			public boolean onBreak(Player p, Block b, SlimefunItem item, UnregisterReason reason) {
+				plugin.removeIndicator(b);
+				return true;
+			}
+		});
+		
+		new BlockMenuPreset(getID(), "&eEnder Chest", true) {
+			
+			@Override
+			public void newInstance(BlockMenu menu, Block b) {
+			}
+					
+			@Override
+			public void init() {
+				setSize(size);
+						
+				addMenuOpeningHandler(new MenuOpeningHandler() {
+					
+					@Override
+					public void onOpen(Player p) {
+						p.playSound(p.getLocation(), Sound.BLOCK_ENDER_CHEST_OPEN, 1.8F, 1.6F);
+					}
+					
+				});
+						
+				addMenuCloseHandler(new MenuCloseHandler() {
+							
+					@Override
+					public void onClose(Player p) {
+						p.playSound(p.getLocation(), Sound.BLOCK_ENDER_CHEST_CLOSE, 1.8F, 1.6F);
+					}
+				});
+			}
+					
+			@Override
+			public int[] getSlotsAccessedByItemTransport(ItemTransportFlow arg0) {
+				return slots;
+			}
+					
+			@Override
+			public boolean canOpen(Block b, Player p) {
+				plugin.updateIndicator(b, c1, c2, c3, Integer.parseInt(BlockStorage.getLocationInfo(b.getLocation(), "yaw")) + 45);
+				return true;
+			}
+		};
+	}
+
+}
---
+++ src/io/github/thebusybiscuit/coloredenderchests/ColoredEnderChests.java
@@ -0,0 +1,150 @@
+package io.github.thebusybiscuit.coloredenderchests;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.bukkit.Material;
+import org.bukkit.block.Block;
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.Entity;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.EulerAngle;
+
+import me.mrCookieSlime.CSCoreLibPlugin.PluginUtils;
+import me.mrCookieSlime.CSCoreLibPlugin.Configuration.Config;
+import me.mrCookieSlime.CSCoreLibPlugin.general.Inventory.Item.CustomItem;
+import me.mrCookieSlime.CSCoreLibPlugin.general.Inventory.Item.MenuItem;
+import me.mrCookieSlime.CSCoreLibPlugin.general.World.ArmorStandFactory;
+import me.mrCookieSlime.CSCoreLibPlugin.general.World.CustomSkull;
+import me.mrCookieSlime.Slimefun.Objects.Category;
+import me.mrCookieSlime.Slimefun.Objects.Research;
+
+public class ColoredEnderChests extends JavaPlugin {
+	
+	protected Config cfg;
+	protected Map<Integer, String> colors = new HashMap<>();
+	protected Category category;
+	protected Material[] wool = {
+		Material.WHITE_WOOL,
+		Material.ORANGE_WOOL,
+		Material.MAGENTA_WOOL,
+		Material.LIGHT_BLUE_WOOL,
+		Material.YELLOW_WOOL,
+		Material.LIME_WOOL,
+		Material.PINK_WOOL,
+		Material.GRAY_WOOL,
+		Material.LIGHT_GRAY_WOOL,
+		Material.CYAN_WOOL,
+		Material.PURPLE_WOOL,
+		Material.BLUE_WOOL,
+		Material.BROWN_WOOL,
+		Material.GREEN_WOOL,
+		Material.RED_WOOL,
+		Material.BLACK_WOOL
+	};
+	
+	protected double angle = Math.toRadians(345);
+	
+	@Override
+	public void onEnable() {
+		PluginUtils utils = new PluginUtils(this);
+		utils.setupConfig();
+		cfg = utils.getConfig();
+		
+		utils.setupMetrics();
+		utils.setupUpdater(99696, getFile());
+		
+		Research r = new Research(2610, "Colored Ender Chests", 20);
+		Research r2 = new Research(2611, "Big Colored Ender Chests", 30);
+		Research r3 = new Research(2612, "Colored Ender Backpacks", 20);
+		Research r4 = new Research(2613, "Big Colored Ender Backpacks", 30);
+		
+		r.register();
+		r2.register();
+		r3.register();
+		r4.register();
+		
+		colors.put(0, "&rWhite");
+		colors.put(1, "&6Orange");
+		colors.put(2, "&dMagenta");
+		colors.put(3, "&bLight Blue");
+		colors.put(4, "&eYellow");
+		colors.put(5, "&aLime");
+		colors.put(6, "&dPink");
+		colors.put(7, "&8Dark Gray");
+		colors.put(8, "&7Light Gray");
+		colors.put(9, "&3Cyan");
+		colors.put(10, "&5Purple");
+		colors.put(11, "&9Blue");
+		colors.put(12, "&6Brown");
+		colors.put(13, "&2Green");
+		colors.put(14, "&4Red");
+		colors.put(15, "&8Black");
+		
+		category = new Category(new MenuItem(Material.ENDER_CHEST, "&5Colored Ender Chests", 0, "open"), 2);
+		Category category2 = null;
+		try {
+			category2 = new Category(new CustomItem(CustomSkull.getItem("eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvMmEzYjM0ODYyYjlhZmI2M2NmOGQ1Nzc5OTY2ZDNmYmE3MGFmODJiMDRlODNmM2VhZjY0NDlhZWJhIn19fQ=="), "&5Colored Ender Backpacks", "", "&a> Click to open"), 2);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		for (int c1 = 0; c1 < 16; c1++) {
+			for (int c2 = 0; c2 < 16; c2++) {
+				for (int c3 = 0; c3 < 16; c3++) {
+					try {
+						registerEnderChest(category, category2, r, r2, r3, r4, c1, c2, c3);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+		
+	}
+	
+	private void registerEnderChest(Category category, Category category2, Research r, Research r2, Research r3, Research r4, final int c1, final int c2, final int c3) throws Exception {
+		if (cfg.getBoolean("small_chests")) {
+			ColoredEnderChest item = new ColoredEnderChest(this, 27, c1, c2, c3);
+			item.register();
+			r.addItems(item);
+		}
+		
+		if (cfg.getBoolean("big_chests")) {
+			ColoredEnderChest item = new ColoredEnderChest(this, 54, c1, c2, c3);
+			item.register();
+			r2.addItems(item);
+		}
+	}
+	
+	protected void updateIndicator(Block b, int c1, int c2, int c3, int yaw) {
+		removeIndicator(b);
+
+		ArmorStandFactory.createSmall(b.getLocation().add(0.5D, 0.5D -0.23D, 0.5D), new ItemStack(wool[c1]), new EulerAngle(angle, 0F, 0F), (float) yaw);
+		ArmorStandFactory.createSmall(b.getLocation().add(0.5D, 0.5D -0.23D, 0.5D), new ItemStack(wool[c3]), new EulerAngle(angle, 0F, 0F), (float) yaw + 180);
+		
+		if (yaw == 45) {
+			ArmorStandFactory.createSmall(b.getLocation().add(0.5D + 0.275, 0.5D -0.23D, 0.5D), new ItemStack(wool[c2]), new EulerAngle(angle, 0F, 0F), (float) yaw);
+		}
+		else if (yaw == 225) {
+			ArmorStandFactory.createSmall(b.getLocation().add(0.5D - 0.275, 0.5D -0.23D, 0.5D), new ItemStack(wool[c2]), new EulerAngle(angle, 0F, 0F), (float) yaw);
+		}
+		else if (yaw == -45){
+			ArmorStandFactory.createSmall(b.getLocation().add(0.5D, 0.5D -0.23D, 0.5D - 0.275), new ItemStack(wool[c2]), new EulerAngle(angle, 0F, 0F), (float) yaw);
+		}
+		else {
+			ArmorStandFactory.createSmall(b.getLocation().add(0.5D, 0.5D -0.23D, 0.5D + 0.275), new ItemStack(wool[c2]), new EulerAngle(angle, 0F, 0F), (float) yaw);
+		}
+	}
+
+	protected void removeIndicator(Block b) {
+		for (Entity n: b.getChunk().getEntities()) {
+			if (n instanceof ArmorStand) {
+				if (n.getCustomName() == null && b.getLocation().add(0.5D, 0.5D, 0.5D).distanceSquared(n.getLocation()) < 0.5D) {
+					n.remove();
+				}
+			}
+		}
+	}
+}
